53cf7d6d1ab785b3abb8d37bb0aed05c29871745
Hammed Oyedele <itz.harmid@gmail.com>
2021-03-21 18:18:23 +0100

    feat: add gbenga greeting code

---
+++ HelloGbenga.go
@@ -0,0 +1,12 @@
+package helloGCP
+
+import (
+        "fmt"
+        "net/http"
+)
+
+// HelloGbenga is an HTTP Cloud Function to greet Gbenga.
+func HelloGbenga(w http.ResponseWriter, r *http.Request) {  
+        fmt.Fprintf(w, "Hello, Gbenga. Working with a superlative boss like is the best experience of my career. Take my heartiest thanks for your excellent leadership.")
+}
+
--- HelloTomisin.go
+++ HelloTomisin.go
@@ -7,5 +7,5 @@ import (
 
 // HelloTomisin is an HTTP Cloud Function to greet Tomisin.
 func HelloTomisin(w http.ResponseWriter, r *http.Request) {  
-        fmt.Fprintf(w, "Hello, Tomisin. Our very own biggest head!")
+        fmt.Fprintf(w, "Hello, Tomisin. Some people are way too beautiful to be described in words. You’re one of them.")
 }
