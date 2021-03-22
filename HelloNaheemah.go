eefc1cb0cca45b23b8ae85634fe6202e935d93fd
Hammed Oyedele <itz.harmid@gmail.com>
2021-03-22 04:52:26 +0100

    fix: change azeezat to naheemah in the greeting

--- HelloNaheemah.go
+++ HelloNaheemah.go
@@ -7,5 +7,5 @@ import (
 
 // HelloNaheemah is an HTTP Cloud Function to greet Naheemah.
 func HelloNaheemah(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Azeezat. My sweetheart, I must be the luckiest man to have someone like you in my life! Every day of my life is beautiful because of you! You are my everything!")
+	fmt.Fprintf(w, "Hello, Naheemah. My sweetheart, I must be the luckiest man to have someone like you in my life! Every day of my life is beautiful because of you! You are my everything!")
 }
